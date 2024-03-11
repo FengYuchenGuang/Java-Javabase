4d3f0a4e95ae1bcdf8d493f7771b922f51756708
FengYuchenGuang <ankskfp237@163.com>
2024-03-11 19:54:10 +0800

    2024/3/11

--- chapter13/src/Generic_/Generic_info/GenericExercise02.java
+++ chapter13/src/Generic_/Generic_info/GenericExercise02.java
@@ -7,6 +7,10 @@ import java.util.Objects;
 
 /**
  * @author hxz
+ * 如果对某一个对象进行比较，下面是对 brithday 进行比较，放在 MyDate类中
+ * 因此，需要对 brithday 进行比较时，最好就放在该类中
+ * 封装后，将来可维护性和复用性，将大大增强
+ *
  */
 
 /**
@@ -16,7 +20,7 @@ import java.util.Objects;
  * 3) 重写 toString 方法输出 name, sal, birthday
  * 4) MyDate 类包含: private 成员变量 month,day,year；并为每一个属性定义 getter, setter 方法；
  * 5) 创建该类的 3 个对象，并把这些对象放入 ArrayList 集合中（ArrayList 需使用泛型来定义），对集合中的元素进
- * 行排序，并遍历输出：
+ *    行排序，并遍历输出：
  * <p>
  * 排序方式： 调用 ArrayList 的 sort 方法 , * 传入 Comparator 对象[使用泛型]，先按照 name 排序，
  * 如果 name 相同，则按生日日期的先后排序。【即：定制排序】
@@ -28,6 +32,8 @@ public class GenericExercise02 {
         employees.add(new Employee02("milan", 9000, 6,
                 new MyDate(1990, 3, 22)));
         employees.add(new Employee02("hxz", 35000, 18,
+                new MyDate(2001, 4, 26)));
+        employees.add(new Employee02("hxz1", 35000, 18,
                 new MyDate(2001, 4, 26)));
         employees.add(new Employee02("jack", 18000, 38,
                 new MyDate(1977, 8, 6)));
@@ -43,35 +49,46 @@ public class GenericExercise02 {
         employees.sort(new Comparator<Employee02>() {
             @Override
             public int compare(Employee02 o1, Employee02 o2) {
+                //定制排序
+                //先对传入参数进行验证
+                if (!(o1 instanceof Employee02 && o2 instanceof Employee02)){
+                    System.out.println("类型不正确...");
+                    return 0;
+                }
                 //按照姓名长度进行排序（从小到大）
-                int len = o1.getName().length() - o2.getName().length();
+//                int len = o1.getName().length() - o2.getName().length();
+                //String 也有重写comparaTo 方法，长度一样按照 Ascall 码排序
+                //都相同按照长度
+                int len = o1.getName().compareTo(o2.getName());
                 if (len > 0) {
                     return 1;
                 } else if (len < 0) {
                     return -1;
                 }
 
-                //姓名长度相同后, 再按照出生年月日排序（从小到大）
-                //相减大于0，说明年龄小的在后面，因此要调换，return 1
-                int year = o1.getBrithday().getYear() - o2.getBrithday().getYear();
-                int month = o1.getBrithday().getMonth() - o2.getBrithday().getMonth();
-                int day = o1.getBrithday().getDay() - o2.getBrithday().getDay();
-                if (year < 0) {
-                    return 1;
-                } else if (year > 0) {
-                    return -1;
-                } else if (month < 0) {
-                    return 1;
-                } else if (month > 0) {
-                    return -1;
-                } else if (day < 0) {
-                    return 1;
-                } else if (day > 0) {
-                    return -1;
-                }
+                //可以在 MyDate 中实现 C
+//                //姓名长度相同后, 再按照出生年月日排序（从小到大）
+//                //相减大于0，说明年龄小的在后面，因此要调换，return 1
+//                int year = o1.getBrithday().getYear() - o2.getBrithday().getYear();
+//                int month = o1.getBrithday().getMonth() - o2.getBrithday().getMonth();
+//                int day = o1.getBrithday().getDay() - o2.getBrithday().getDay();
+//                if (year < 0) {
+//                    return 1;
+//                } else if (year > 0) {
+//                    return -1;
+//                } else if (month < 0) {
+//                    return 1;
+//                } else if (month > 0) {
+//                    return -1;
+//                } else if (day < 0) {
+//                    return 1;
+//                } else if (day > 0) {
+//                    return -1;
+//                }
 
-                //姓名长度，出生日期都相同则不变
-                return 0;
+                //姓名长度相同后, 再按照出生年月日排序（从小到大）
+                return o1.getBrithday().compareTo(o2.getBrithday());
+
             }
         });
 
@@ -163,7 +180,7 @@ class Employee02 {
     }
 }
 
-class MyDate {
+class MyDate implements Comparable<MyDate>{
     private int year;
     private int month;
     private int day;
@@ -172,6 +189,27 @@ class MyDate {
         this.year = year;
         this.month = month;
         this.day = day;
+    }
+
+    @Override
+    public int compareTo(MyDate o) {
+        //姓名长度相同后, 再按照出生年月日排序（从小到大）
+        //相减大于0，说明年龄小的在后面，因此要调换，return 1
+        int yearMins = year - o.getYear();
+        if (yearMins != 0 ){
+            return -yearMins;
+        }
+        //如果year 相同
+        int monthMins = month-o.getMonth();
+        if (monthMins!=0){
+            return -monthMins;
+        }
+        //如果month 相同
+        int dayMins = day-o.getDay();
+        if (dayMins!=0){
+            return -dayMins;
+        }
+        return 0;
     }
 
     public String Bri() {
