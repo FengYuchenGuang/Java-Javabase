e9389e0db00adfae9b1f1a54e02131d228863aee
FengYuchenGuang <ankskfp237@163.com>
2024-03-12 23:22:27 +0800

    2024/3/12

---
+++ TankWar/src/studay/EventProcessing/MoveTank/LoadTankMove.java
@@ -0,0 +1,10 @@
+package studay.EventProcessing.MoveTank;
+
+import javax.swing.*;
+
+/**
+ * @author hxz
+ */
+public class LoadTankMove extends JFrame {
+
+}
---
+++ TankWar/src/studay/EventProcessing/MoveTank/MoveTank.java
@@ -0,0 +1,13 @@
+package studay.EventProcessing.MoveTank;
+
+/**
+ * @author hxz
+ */
+public class MoveTank {
+    public static void main(String[] args) {
+        LoadTankMove loadTankMove = new LoadTankMove();
+
+    }
+}
+
+
