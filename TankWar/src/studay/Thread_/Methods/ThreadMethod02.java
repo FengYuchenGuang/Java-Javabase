aae2c0f9ea1f0d2ad0a3dca2fa3e244610ff74a9
FengYuchenGuang <ankskfp237@163.com>
2024-03-13 23:59:52 +0800

    2024/3/13

--- TankWar/src/studay/Thread_/Methods/ThreadMethod02.java
+++ TankWar/src/studay/Thread_/Methods/ThreadMethod02.java
@@ -6,7 +6,43 @@ package studay.Thread_.Methods;
  * 2、join   线程插队
  */
 public class ThreadMethod02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        T02 t02 = new T02();
+        t02.setName("hxz");
+        t02.start();
+
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(1000);
+            //Thread.currentThread().getName() 获取线程名
+            System.out.println("线程 " + Thread.currentThread().getName() + " 运行中..." + i);
+            if (i==4){
+                System.out.println("小弟已经运行5次，大哥先运行...");
+                //2、join   线程插队
+                t02.join();
+                System.out.println("大哥运行完...");
+
+            }
+        }
+    }
+}
+
+class T02 extends Thread {
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 20; i++) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //Thread.currentThread().getName() 获取线程名
+            System.out.println("线程 " + Thread.currentThread().getName() + " 运行中..." + i);
+        }
+
+
 
     }
+
 }
