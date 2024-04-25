750168c7e7cf18c70672391e43f2eeea1f3d4cac
FengYuchenGuang <ankskfp237@163.com>
2024-04-26 00:16:47 +0800

    2024/4/26

--- chapter12/src/collect_/List_/ArrayDeque_.java
+++ chapter12/src/collect_/List_/ArrayDeque_.java
@@ -5,6 +5,14 @@ import java.util.ArrayDeque;
 /**
  * @author hxz
  * ArrayDeque 双端队列
+ * ArrayDeque是 Deque接口的一个实现，使用了可变数组，所以没有容量上的限制。
+ *      同时， ArrayDeque是线程不安全的，在没有外部同步的情况下，不能再多线程环境下使用。
+ *
+ * ArrayDeque是 Deque的实现类，可以作为栈来使用，效率高于 Stack；也可以作为队列来使用，效率高于 LinkedList。
+ *
+ * ArrayDeque 是 Java 集合中双端队列的数组实现，双端队列的链表实现（LinkedList）
+ *
+ * 需要注意的是， ArrayDeque不支持 null值。
  */
 public class ArrayDeque_ {
     public static void main(String[] args) {
