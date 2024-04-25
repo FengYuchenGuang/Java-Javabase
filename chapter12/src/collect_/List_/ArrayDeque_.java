8c35525132d7da395daf3b138393e6ee2d325aa9
FengYuchenGuang <ankskfp237@163.com>
2024-04-26 00:15:48 +0800

    2024/4/26

---
+++ chapter12/src/collect_/List_/ArrayDeque_.java
@@ -0,0 +1,13 @@
+package collect_.List_;
+
+import java.util.ArrayDeque;
+
+/**
+ * @author hxz
+ * ArrayDeque 双端队列
+ */
+public class ArrayDeque_ {
+    public static void main(String[] args) {
+
+    }
+}
